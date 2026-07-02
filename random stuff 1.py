4e619c5b0ea904bf74b8f8622be886830e974c09
CodejitsuLiam <kellyargento@gmail.com>
2026-07-02 09:09:54 -0700

    New bugfix

--- random stuff 1.py
+++ random stuff 1.py
@@ -8,6 +8,7 @@ verb2OG = "attack"
 nounOG = "book"
 noun2OG = "sword"
 noun3OG = "castle"
+#This is the way to make things interactive. Haven't figured out a way to turn it off yet.
 name=input("Type a name and press enter: ")
 adjective=input("Type an adjective and press enter: ")
 name2=input("Type another name and press enter: ")
@@ -19,9 +20,10 @@ noun=input("Type a noun and press enter: ")
 noun2=input("Type another noun and press enter: ")
 noun3=input("Type another noun and press enter: ")
 
-print("Once upon a time, there was a person named " + name + ". " + name + " was " + adjective + ".")
+
+print("\t" + "Once upon a time, there was a person named " + name + ". " + name + " was " + adjective + ".")
 print("There was also a person named " + name2 + ". " + name2 + " was " + adjective2 + ".")
 print("One day, the " + noun3 + " was too " + adjective3 + " so " + name + " and " + name2 + " decided to " + verb2 + " an enemy " + noun3 + ".")
 print("The " + verb2 + " failed terribly, and they were forced to " + verb + ".")
 print("After returning, " + name + " found a " + noun + " that taught the secrets of the universe to whoever read it.")
-print(name + " called everyone to gather, and raised the " + noun2 + " of the kingdom to the sky, and declared that everyone shall live in peace.")
+sys.exit()
