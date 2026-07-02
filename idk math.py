31e3a19b6dc452a3d8874ad63fcb7492c1d84587
CodejitsuLiam <kellyargento@gmail.com>
2026-07-02 09:01:03 -0700

    I added this idk

---
+++ idk math.py
@@ -0,0 +1,9 @@
+code1 = 3897
+code2 = 901873
+step1 = code1 + code2
+step2 = step1 * 2
+print("Your final code is", step2)
+code3= 40
+code4= 5
+step3 = code3 + code4
+print("Your other code is", step3)
---
+++ random stuff 1.py
@@ -0,0 +1,27 @@
+nameOG = "Bobificus"
+adjectiveOG = "amazing"
+name2OG = "Maximillius Pontrificus the 2nd"
+adjective2OG = "very goofy"
+adjective3OG = "peaceful"
+verbOG = "run"
+verb2OG = "attack" 
+nounOG = "book"
+noun2OG = "sword"
+noun3OG = "castle"
+name=input("Type a name and press enter: ")
+adjective=input("Type an adjective and press enter: ")
+name2=input("Type another name and press enter: ")
+adjective2=input("Type another adjective and press enter: ")
+adjective3=input("Type another adjective and press enter: ")
+verb=input("Type a verb and press enter: ")
+verb2=input("Type another verb and press enter: ")
+noun=input("Type a noun and press enter: ")
+noun2=input("Type another noun and press enter: ")
+noun3=input("Type another noun and press enter: ")
+
+print("Once upon a time, there was a person named " + name + ". " + name + " was " + adjective + ".")
+print("There was also a person named " + name2 + ". " + name2 + " was " + adjective2 + ".")
+print("One day, the " + noun3 + " was too " + adjective3 + " so " + name + " and " + name2 + " decided to " + verb2 + " an enemy " + noun3 + ".")
+print("The " + verb2 + " failed terribly, and they were forced to " + verb + ".")
+print("After returning, " + name + " found a " + noun + " that taught the secrets of the universe to whoever read it.")
+print(name + " called everyone to gather, and raised the " + noun2 + " of the kingdom to the sky, and declared that everyone shall live in peace.")
