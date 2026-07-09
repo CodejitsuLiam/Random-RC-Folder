18dea256d94b0e6dcc976bf280f897b250b6670c
CodejitsuLiam <kellyargento@gmail.com>
2026-07-09 08:55:17 -0700

    had ideas, made adventure

---
+++ testingtuples.py
@@ -0,0 +1,8 @@
+student = {"name": "Alex", "grade": 8, "school": "Lincoln", "subjects": ["Band", "LA", "Spanish", "Math", "Science", "History"]}
+print("Student name:", student["name"])
+print("Student grade:", student["grade"])
+print("Student school:", student["school"])
+print("Student subjects:", student["subjects"])
+student["subjects"].pop
+print("Student subjects after removal", student["subjects"])
+
---
+++ textadventure.py
@@ -0,0 +1,107 @@
+import time
+import random
+import sys
+inventory=[]
+cave={"location": "north of the village", "description": "an ominous cave that looks like a gaping mouth", "itemsneeded": "a torch"}
+village={"description": "the only place you have ever known"}
+man={"clothesdescription": "ragged, torn, and speckled with dirt and blood", "description": "covered in cuts and scrapes from head to toe, and when he moves, he does so with great effort and pain", "words": "I have not long on this fair earth, but it is possible you can avenge me. There is a cave north of the village, where I met my untimely fate. Go there and defeat that which has defeated me."}
+def endignore():
+    print("You decide to forget about everything that has happened today. Despite the death of the man you met at the edge of the woods, there is nothing in it for you to risk what happened to him. ")
+    time.sleep(5)
+    print("\033[31mThe end.\033[0m")
+    sys.exit()
+def dialogue():
+    print(f"You live in a small village, which is {village["description"]}.")
+    time.sleep(4)
+    print(f'One day, you see a man stumble out of woods.')
+    time.sleep(2)
+    print(f'His clothes are {man["clothesdescription"]}. He himself is in a more pitiful state- he is {man["description"]}.')
+    time.sleep(7)
+    print(f'You realize, with some surprise, that you do not recognize him. You know that there are other villages and towns besides yours, but without much outside contact, sometimes it feels as if your village is the only one in the world.')
+    time.sleep(7)
+    print(f'He sees you and says, "{man["words"]}" With these words, he gives a groan of despair, then falls at your feet, dead.')
+    time.sleep(9)
+def preparation():
+    togoornottogo=input("Back inside, you think about what happened. Waiting in front of you is a tantalizing adventure, but is it worth risking the fate of that poor soul? ")
+    if togoornottogo in ("yes", "y"):
+        print("It is worth the risk. Your whole life, you have wanted a greater purpose, and you have finally found one.")
+        time.sleep(5)
+    elif togoornottogo in ("no", "n"):
+        endignore()
+    while True:
+        weapongrab=input("Having decided to go to the cave, you realize you may need a weapon. You see a kitchen knife on the cutting board. Do you wish to grab it to defend yourself? ").strip().lower()
+        if weapongrab in ("yes", "y"):
+            inventory.append("Knife")
+            print("You decide to grab the knife, knowing it may come in handy. ")
+            break
+        if weapongrab in ("no", "n"):
+            print("Are you sure you don't want to grab the knife? ")
+            time.sleep(2)
+dialogue()
+preparation()      
+def entercave():
+    print("You enter the cave...")
+    import time
+    time.sleep(3)
+def maybeattacked():
+    import random
+    attackcaveevents=["Nothing happens","A monstrous spider",]
+    attackcaveweights=[50, 50]
+    attackcave=random.choices(attackcaveevents, weights=attackcaveweights, k=1)[0]
+    if attackcave=="Nothing happens":
+        pass
+    if attackcave=="A monstrous spider":
+        firstattack=input("\033[31mA monstrous spider leaps out of the shadows! What will you do? \033[0m(Run or Fight)")
+        #more to work on later
+def outnoise():
+    outnoise=input("As you walk away from the cave, you here an anguished scream come from the mouth of the cave. You spin around, but see nothing. The scream sounded human, but if someone is screaming like that, something must have caused it. Is it worth the risk to go into the cave and save the poor soul within? ").strip().lower()
+    if outnoise in ("y","yes"):
+        entercave()
+        maybeattacked()
+        print("You continue deeper into the cave, searching for the source of the scream.")
+        time.sleep(4)
+        print("You see droplets of blood, and you follow them until they suddenly curve into a tunnel at your right. You peek down the tunnel, but it curves along the way, so you cannot see the end. ")
+        time.sleep(8)
+    elif outnoise in ("n","no"):
+        wheregonow=input("It's not worth the risk. Do you wish to return to your village or keep exploring? (Enter Return or Explore) ").strip().lower()
+        if wheregonow=="return":
+            endignore()
+        elif wheregonow=="explore":
+            pass#work on this l8r
+        caveexpplored=False
+def innoise():
+    print(f"After {killorescape} the spider, you continue deeper into the cave. ")
+    time.sleep(3)
+    print("Suddenly, you hear a loud and anguished scream from a tunnel at your right. You peek down the tunnel, but it curves along the way, so you cannot see the end. ")
+    time.sleep(6)
+caveenter = input(f"You see {cave["description"]}. Do you go in? ").strip().lower()
+def cavesequence():
+    while True:
+        if caveenter in ("yes", "y"):
+            entercave()
+            maybeattacked()
+            innoise()
+        elif caveenter in ("n", "no"):
+            print("You decide not to enter the cave right now. ")
+            outnoise()
+        else:
+            print("Please type either yes or no")
+def encounter():
+    while True:
+        enterorwalk=input("Do you enter the tunnel to see what lurks inside, do you continue exploring the caves, or do you leave the cave entirely? (Enter, Explore or Leave) ").strip().lower()
+        if enterorwalk=="explore":
+            print("You decide to leave for now and come back later, when you are feeling more confident.")
+            break
+            #more to work on l8r
+        elif enterorwalk=="enter":
+            print("You creep along the tunnel, walking stealthily as to not alert anything of your presence. Turning the corner, you are confused, as at the end of the tunnel is the man you met before! He faces away from you, but you recognize the tattered clothing and posture. ")
+            time.sleep(10)
+
+            break
+        elif enterorwalk=="leave":
+            break#sync w/ wheregonow=explore
+        else:
+            print("You cannot do that!")
+            time.sleep(2)
+cavesequence()
+encounter()
